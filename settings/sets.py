bd08f6620cbcc922d1683188525f429a362428ca
anatt <anattoliy966@gmail.com>
2023-11-19 23:01:03 +0200

    add user_data

--- settings/sets.py
+++ settings/sets.py
@@ -1,8 +1,8 @@
 PROD_SERVER = "https://good-book.com.ua/ua"
 TEST_EMAIL = "test.mail@mail.com"
 PASSWORD = "Qwerty54321"
-FIRSTNAME = "Usertest"
-LASTNAME = "Userstests"
+FIRSTNAME = "test"
+LASTNAME = "tests"
 PHONE = "+380501234567"
 
 IMPLICITLY_WAIT = 10 #неочевидные ожидания (секунды)
